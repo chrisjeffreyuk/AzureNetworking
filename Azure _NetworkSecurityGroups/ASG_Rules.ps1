87409c3879cf6674aeb09c488c43740bbd58fc0f
Chris Jeffrey <chrisjeffrey666@gmail.com>
2018-10-02 16:29:57 +0100

    Inintial Upload

---
+++ Azure _NetworkSecurityGroups/ASG_Rules.ps1
@@ -0,0 +1,37 @@
+﻿Login-AzureRmAccount
+Get-AzureRmSubscription
+Set-AzureRmContext -SubscriptionId "xxxx"
+
+# Declare the vairables
+$rgName = "demoRG"
+$vnetName = "demoRG-vnet"
+$nsgName = "demoNSG01"
+$location = "EastUS2"
+$sourcePort = "*"
+$destinationPort = 80,22
+$priority = "106"
+
+# Create the Application Security Group
+$asg1 = New-AzureRmApplicationSecurityGroup -ResourceGroupName $rgName -Name WebServers -Location $location
+
+Get-AzureRmApplicationSecurityGroup
+
+# Get the NSG resource
+$resource = Get-AzureRmResource | Where {$_.ResourceGroupName –eq $rgName -and $_.ResourceType -eq "Microsoft.Network/networkSecurityGroups"} 
+$nsg = Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName
+
+# Create the Application Security Group Rule
+Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName |
+Add-AzureRmNetworkSecurityRuleConfig -Name WebServers -Description "Custom ASG Rule" -Access Allow `
+    -Protocol "*" -Direction Inbound -Priority $priority -SourceApplicationSecurityGroup $asg1 -SourcePortRange $sourcePort `
+    -DestinationApplicationSecurityGroup $asg1 -DestinationPortRange $destinationPort |
+Set-AzureRmNetworkSecurityGroup
+
+# Apply the Application Security Group to the virtual machine interfaces
+$nic = Get-AzureRmNetworkInterface -Name demoVM01-NIC -ResourceGroupName $rgName
+$nic.IpConfigurations[0].ApplicationSecurityGroups = $asg1
+Set-AzureRmNetworkInterface -NetworkInterface $nic
+
+$nic = Get-AzureRmNetworkInterface -Name demoVM02-NIC -ResourceGroupName $rgName
+$nic.IpConfigurations[0].ApplicationSecurityGroups = $asg1
+Set-AzureRmNetworkInterface -NetworkInterface $nic
---
+++ Azure _NetworkSecurityGroups/NSG_Rules.ps1
@@ -0,0 +1,26 @@
+﻿Login-AzureRmAccount
+Get-AzureRmSubscription
+Set-AzureRmContext -SubscriptionId "xxxx"
+
+# Declare the vairables
+$rgName = "demoRG"
+$source = "*"
+$sourcePort = "*"
+$destinationPort = "80"
+$rulename = "Custom_Allow_Port_80"
+$nsgName = "demoNSG01"
+$priority = "105"
+$sourceIP = "*"
+$destinationIP = "10.1.0.12"
+
+# Get the NSG resource
+$resource = Get-AzureRmResource | Where {$_.ResourceGroupName –eq $rgName -and $_.ResourceType -eq "Microsoft.Network/networkSecurityGroups"} 
+$nsg = Get-AzureRmNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName
+
+# Add the inbound security rule.
+$nsg | Add-AzureRmNetworkSecurityRuleConfig -Name $rulename -Description "Allow Port" -Access Allow `
+    -Protocol * -Direction Inbound -Priority $priority -SourceAddressPrefix $source -SourcePortRange $sourcePort `
+    -DestinationAddressPrefix $destinationIP -DestinationPortRange $destinationPort
+
+# Update the NSG.
+$nsg | Set-AzureRmNetworkSecurityGroup
